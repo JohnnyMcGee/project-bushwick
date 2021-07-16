ec9faf2410e94a43a4caf4bddd34023ae8a52cab
Johnny McGee <johnnymcgee.dev@gmail.com>
2021-07-15 21:08:18 -0400

    Add post and delete functions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const items = new Set();
+app.use(express.json());
+
 app.get("/", (_, res) => {
-    res.send("Cowabunga, dude!");
+    res.status(200).json(Array.from(items));
+});
+
+app.post("/", (req, res) => {
+    
+    const { item } = req.body;
+
+    if (typeof item !== "string") {
+        res.status(400).send("invalid item");
+    } 
+    else {
+        items.add(item);
+        res.status(200).send();
+    }
+});
+
+app.delete("/",  (req, res) => {
+    const {item} = req.body;
+    if (!items.has(item)) {
+        res.status(400).send("item not found");
+    }
+    else {
+        items.delete(item);
+        res.status(200).send();
+    }
 });
 
 app.listen(port, () => {
